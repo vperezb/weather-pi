e33714714064d4dd1edd8de8d2f722998b884697
vperezb <victor.perez.berruezo@gmail.com>
2021-11-14 12:27:30 +0100

    typo on bool evaluation

--- rasp/joystick.py
+++ rasp/joystick.py
@@ -7,7 +7,7 @@ sense = SenseHat()
 
 while True:
     for event in sense.stick.get_events():
-        if (event.direction == 'up' && event.action == 'pressed':
+        if (event.direction == 'up' & event.action == 'pressed'):
             sense.set_pixel(7, 7, 0, 255, 255)
             registry_uploader.upload_to_storage()
             sense.clear(124,252,0)
